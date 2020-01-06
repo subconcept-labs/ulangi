cb5542b9a0d65ef0ebabcc51fa7fe69412b8de2a
minhloi <jimmyloi92@yahoo.com>
2020-01-05 19:53:11 -0800

    fix: move shebang line above copyright notice

--- packages/ulangi-script/src/commands/uls-database-drop.ts
+++ packages/ulangi-script/src/commands/uls-database-drop.ts
@@ -1,11 +1,11 @@
+#!/usr/bin/env node
+
 /*
  * Copyright (c) Minh Loi.
  *
  * This file is part of Ulangi which is released under GPL v3.0.
  * See LICENSE or go to https://www.gnu.org/licenses/gpl-3.0.txt
  */
-
-#!/usr/bin/env node
 
 import { DatabaseManagerFacade } from '@ulangi/ulangi-remote-database';
 import { default as chalk } from 'chalk';
--- packages/ulangi-script/src/commands/uls-library.ts
+++ packages/ulangi-script/src/commands/uls-library.ts
@@ -1,11 +1,11 @@
+#!/usr/bin/env node
+
 /*
  * Copyright (c) Minh Loi.
  *
  * This file is part of Ulangi which is released under GPL v3.0.
  * See LICENSE or go to https://www.gnu.org/licenses/gpl-3.0.txt
  */
-
-#!/usr/bin/env node
 
 import * as program from 'commander';
 
