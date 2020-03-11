26d0fa76d1a43662a30a53748d9acda7f7ac8e66
minhloi <jimmyloi92@yahoo.com>
2020-03-11 14:10:50 -0700

    fix: use custom build sqlite for Android

--- packages/ulangi-mobile/react-native.config.js
+++ packages/ulangi-mobile/react-native.config.js
@@ -3,5 +3,17 @@ module.exports = {
     ios: {},
     android: {},
   },
+  dependencies: {
+    "react-native-sqlite-storage": {
+      platforms: {
+        android: {
+          sourceDir:
+            "../node_modules/react-native-sqlite-storage/platforms/android-native",
+          packageImportPath: "import io.liteglue.SQLitePluginPackage;",
+          packageInstance: "new SQLitePluginPackage()"
+        }
+      }
+    }
+  },
   assets: ['./assets/fonts/']
 };
